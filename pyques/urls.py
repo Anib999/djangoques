b33f0d746444a3f3988f5976d7393a54b765ba4a
Anib <saliv.mah@gmail.com>
2022-09-14 21:46:17 +0545

    oop

--- pyques/urls.py
+++ pyques/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.simpleques, name="simpleques")
+    path('', views.simpleques, name="simpleques"),
+    path('simplequesans/', views.simplequesans, name="simplequesans"),
+    path('oop/', views.oop, name="oop"),
+    path('oopans/', views.oopans, name="oopans"),
 ]
--- pyques/views.py
+++ pyques/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def simpleques(request):
     return render(request, 'pyques/simpleques.html')
+
+def simplequesans(request):
+    return render(request, 'pyques/simplequesns.html')
+
+def oop(request):
+    return render(request, 'pyques/oop.html')
+
+def oopans(request):
+    return render(request, 'pyques/oopans.html')
